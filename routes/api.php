cf4f125f207747942bca543760b458b0e8a7c6cd
NaingWinAung <naingwinaung1710@gmail.com>
2026-02-24 23:25:41 +0700

    prevent api with throttle

--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,14 @@
 use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/register', [AuthController::class, 'register']);
+Route::middleware('throttle:api')->group(function () {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
 
-Route::post('/oauth/{provider}/callback', [AuthController::class, 'handleProviderCallback']);
+    Route::post('/oauth/{provider}/callback', [AuthController::class, 'handleProviderCallback']);
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/user', [AuthController::class, 'user']);
-    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::get('/user', [AuthController::class, 'user']);
+        Route::post('/logout', [AuthController::class, 'logout']);
+    });
 });
