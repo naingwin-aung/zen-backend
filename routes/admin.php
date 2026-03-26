ba0428aae2803274a1e29ad0c5258ca68b73c830
NaingWinAung <naingwinaung1710@gmail.com>
2026-03-27 00:14:37 +0700

    update to laravel 13 with laravel boost

--- routes/admin.php
+++ routes/admin.php
@@ -6,16 +6,16 @@ use App\Http\Controllers\Admin\CountryController;
 use App\Http\Controllers\Admin\CruiseController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('throttle:api')->group(function () {
-    Route::post('/login', [AuthController::class, 'login']);
+// Route::middleware('throttle:api')->group(function () {
+Route::post('/login', [AuthController::class, 'login']);
 
-    Route::middleware('auth:sanctum')->group(function () {
-        Route::get('/user', [AuthController::class, 'user']);
-        Route::post('/logout', [AuthController::class, 'logout']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', [AuthController::class, 'user']);
+    Route::post('/logout', [AuthController::class, 'logout']);
 
-        Route::apiResource('countries', CountryController::class);
-        Route::apiResource('categories', CategoryController::class);
+    Route::apiResource('countries', CountryController::class);
+    Route::apiResource('categories', CategoryController::class);
 
-        Route::apiResource('cruises', CruiseController::class);
-    });
+    Route::apiResource('cruises', CruiseController::class);
 });
+// });
