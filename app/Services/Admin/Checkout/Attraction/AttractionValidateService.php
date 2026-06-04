d68f2fea14f630f78bd08d73e756652ea41245fc
NaingWinAung <naingwinaung1710@gmail.com>
2026-06-04 21:08:23 +0700

    please select date add

--- app/Services/Admin/Checkout/Attraction/AttractionValidateService.php
+++ app/Services/Admin/Checkout/Attraction/AttractionValidateService.php
@@ -12,7 +12,7 @@ class AttractionValidateService implements ValidationInterface
      * @param  array<string, string>  $rules
      * @param  array<string, string>  $messages
      */
-    public function handle(int|string $key, array &$rules, array &$messages): void
+    public function handle(int|string $key, array &$rules, array &$messages) : void
     {
         $this->rules($key, $rules);
         $this->messages($key, $messages);
@@ -23,10 +23,11 @@ class AttractionValidateService implements ValidationInterface
      *
      * @param  array<string, string>  $rules
      */
-    private function rules(int|string $key, array &$rules): void
+    private function rules(int|string $key, array &$rules) : void
     {
         $rules["products.$key.product_id"] = 'required|integer';
         $rules["products.$key.package_id"] = 'required|integer';
+        $rules["products.$key.date"] = 'required|date|after_or_equal:today';
         $rules["products.$key.quantities"] = 'required|array';
         $rules["products.$key.quantities.*.id"] = 'required|integer';
         $rules["products.$key.quantities.*.quantity"] = 'required|integer|min:1';
@@ -37,12 +38,15 @@ class AttractionValidateService implements ValidationInterface
      *
      * @param  array<string, string>  $messages
      */
-    private function messages(int|string $key, array &$messages): void
+    private function messages(int|string $key, array &$messages) : void
     {
         $messages["products.$key.product_id.required"] = 'Product ID is required.';
         $messages["products.$key.product_id.integer"] = 'Product ID must be an integer.';
         $messages["products.$key.package_id.required"] = 'Package ID is required.';
         $messages["products.$key.package_id.integer"] = 'Package ID must be an integer.';
+        $messages["products.$key.date.required"] = 'Please select the date';
+        $messages["products.$key.date.date"] = 'Date must be a valid date.';
+        $messages["products.$key.date.after_or_equal"] = 'Date must be after or equal to today.';
         $messages["products.$key.quantities.required"] = 'Quantities is required.';
         $messages["products.$key.quantities.array"] = 'Quantities must be an array.';
         $messages["products.$key.quantities.*.id.required"] = 'ID is required.';
