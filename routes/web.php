16b5c287ba4ae7ed6c48c7ae6ed47a7ed578bbd2
NaingWinAung <naingwinaung1710@gmail.com>
2026-06-30 17:47:11 +0700

    backend is running

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ApplicationController;
 
 Route::get('image/{filename}', [ApplicationController::class, 'image'])->where('filename', '.*');
+
+Route::get('/', function () {
+    return "Backend is running";
+});
